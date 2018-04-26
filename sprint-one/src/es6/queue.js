ab430e96f580ee02e6284b9bf0c1f2e290e6e629
Jehwa Shin <s.jehwa@gmail.com>
2018-04-25 23:24:05 -0700

    finished 'The es6 pattern'

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,25 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.deleted;
+  }
+  enqueue(val) {
+    for(let i = Object.keys(this.storage).length; i > 0; i --){
+      this.storage[i] = this.storage[i-1];
+    }
+    this.storage["0"] = val;
   }
 
+  dequeue() {
+    if(Object.keys(this.storage).length >= 0) {
+      this.deleted = this.storage[Object.keys(this.storage).length-1];
+      delete this.storage[Object.keys(this.storage).length-1];
+      return this.deleted;
+    }
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
