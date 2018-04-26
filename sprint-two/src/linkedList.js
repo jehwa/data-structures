b7353c13dd92f1a6a91291bb7c959866843ffafc
jehwa <s.jehwa@gmail.com>
2018-04-26 16:02:24 -0700

    linkedList finished

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,46 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+   
 
   list.addToTail = function(value) {
+    
+    // debugger;
+    var newNode = new Node(value);
+    if (!this.head && !this.tail) {
+      this.head = newNode;
+    }
+    if (this.tail) {
+      this.tail.next = newNode;
+    }
+    this.tail = newNode;
+    // node.value = value;
+     
   };
 
   list.removeHead = function() {
+    var previousHead = this.head.value;
+    if (this.head) {
+      this.head = this.head.next;
+    }
+    return previousHead;
+    
   };
 
   list.contains = function(target) {
+    var output = false;
+    var findTarget = function(nodes) {
+      if (nodes.value === target) {
+        output = true;
+      }
+      if (nodes.next) {
+        findTarget(nodes.next);
+      }
+    };
+    findTarget(this.head);
+    
+    return output;
+    
   };
 
   return list;
