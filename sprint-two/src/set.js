f999c254752bee83cc9f0a14311e12500d525eac
Jehwa Shin <s.jehwa@gmail.com>
2018-04-28 09:56:49 -0700

    finished set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,38 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  // debugger;
+  this._storage.push(item);
+  console.log(this._storage);
 };
 
 setPrototype.contains = function(item) {
+  let output = false;
+  // debugger;
+  if (this._storage.length) {
+    _.each(this._storage, function(val) {
+      if (val === item) {
+        output = true;
+      }
+    });
+  }
+  return output;
 };
 
 setPrototype.remove = function(item) {
+  if (this._storage) {
+    for (let i = 0; i < this._storage.length; i ++) {
+      if (this._storage[i] === item) {
+        this._storage.splice(i, 1);
+      }
+    }
+  }
 };
 
 /*
