80b3faecfaaf151dfa233c411675f68caeb6e67e
Jehwa Shin <s.jehwa@gmail.com>
2018-04-28 09:43:03 -0700

    finished binarySearchTree

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,42 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  // list.storage = {};
 
   list.addToTail = function(value) {
+    var newVal = new Node(value);
+    // storage[newValvalue] = value;
+    if (this.tail) {
+      this.tail.next = newVal;
+    }
+    if (this.head === null) {
+      this.head = newVal;
+    }
+    this.tail = newVal;
   };
 
   list.removeHead = function() {
+    var previousHead = list.head;
+    if (list.head) {
+      list.head = list.head.next;
+    }
+    return previousHead.value;
   };
 
   list.contains = function(target) {
+    var output = false;
+    // debugger;
+    var findTarget = function(head) {
+      if (head.value === target) {
+        output = true;
+      }
+      if (head.next) {
+        findTarget(head.next);
+      }
+    };
+
+    findTarget(this.head);
+    return output;
   };
 
   return list;
